bca9b95db5497bc3d2b1e68fad603bc07bd9235d
Oskar <oskarad.2000@gmail.com>
2020-11-02 14:59:11 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# Main file of our application
+# Here we will initialise core widgets of application as well as whole app
+
+#  Documentation of Tkinter can be found here: https://docs.python.org/3/library/tkinter.html
+
